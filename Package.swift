3ecc23562db6ceacb40fee35ac23ff5f89ac7f7b
Simon Støvring <mail@simonbs.dk>
2022-06-27 15:03:34 +0200

    Adds swift-docc-plugin

--- Package.swift
+++ Package.swift
@@ -13,3 +13,10 @@ let package = Package(
         .target(name: "KeyboardToolbar", resources: [.process("Assets.xcassets")])
     ]
 )
+
+#if swift(>=5.6)
+// Add the documentation compiler plugin if possible
+package.dependencies.append(
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
+)
+#endif
