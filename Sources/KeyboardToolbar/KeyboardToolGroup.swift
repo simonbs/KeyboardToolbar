2dd503c4ec9b8fd0b35b5b83c76c552676f73ee3
Simon Støvring <mail@simonbs.dk>
2022-06-27 12:21:59 +0200

    Fixes argument order

--- Sources/KeyboardToolbar/KeyboardToolGroup.swift
+++ Sources/KeyboardToolbar/KeyboardToolGroup.swift
@@ -4,19 +4,19 @@ import Foundation
 ///
 /// Tool groups are evenly distributed in the toolbar. Use ``KeyboardToolGroup/spacing`` to specify the spacing within the group.
 public struct KeyboardToolGroup {
-    /// The items to be displayed in the group.
-    public let items: [KeyboardToolGroupItem]
     /// The spacing between items in the group.
     ///
     /// The default value is 6.
     public let spacing: CGFloat
+    /// The items to be displayed in the group.
+    public let items: [KeyboardToolGroupItem]
 
     /// Initializes a tool group.
     /// - Parameters:
-    ///   - items: The items to be displayed in the group.
     ///   - spacing: The spacing between items in the group. Defaults to 6.
-    public init(items: [KeyboardToolGroupItem], spacing: CGFloat = 6) {
-        self.items = items
+    ///   - items: The items to be displayed in the group.
+    public init(spacing: CGFloat = 6, items: [KeyboardToolGroupItem]) {
         self.spacing = spacing
+        self.items = items
     }
 }
