ae1efbfa73a88f8caabac62234973a0fbe0cfde2
Simon Støvring <mail@simonbs.dk>
2025-08-31 13:40:11 +0200

    Adds CGPath.keyboardToolPickerBackground(with:)

---
+++ Sources/KeyboardToolbar/Helpers/CGPath+Helpers.swift
@@ -0,0 +1,128 @@
+import UIKit
+
+struct KeyboardToolPickerBackgroundPathConfiguration {
+    let bounds: CGRect
+    let shadowBlur: CGFloat
+    let handleSize: CGSize
+    let preferredHandleXPosition: CGFloat
+    let handleShadowLength: CGFloat
+    let plateRadius: CGFloat
+    let handleRadius: CGFloat
+
+    fileprivate var insetBounds: CGRect {
+         bounds.inset(by: UIEdgeInsets(top: shadowBlur, left: shadowBlur, bottom: 0, right: shadowBlur))
+    }
+    var handleRect: CGRect {
+        let handleXPosition = preferredHandleXPosition
+        let handleOrigin = CGPoint(x: handleXPosition, y: insetBounds.maxY - handleSize.height - shadowBlur - handleShadowLength)
+        return CGRect(origin: handleOrigin, size: handleSize)
+    }
+}
+
+extension CGPath {
+    static func keyboardToolPickerBackground(with config: KeyboardToolPickerBackgroundPathConfiguration) -> CGPath {
+        let drawingTechniqueBufferDistance = config.handleSize.width * 0.8
+
+        let pPlateTopLeftCorner = CGPoint(x: config.insetBounds.minX, y: config.insetBounds.minY)
+        let pPlateTopRightCorner = CGPoint(x: config.insetBounds.maxX, y: config.insetBounds.minY)
+        let pPlateBottomRightCorner = CGPoint(x: config.insetBounds.maxX, y: config.handleRect.minY)
+        let pPlateBottomLeftCorner = CGPoint(x: config.insetBounds.minX, y: config.handleRect.minY)
+
+        let pHandleTopRightCorner = CGPoint(x: config.handleRect.maxX, y: config.handleRect.minY)
+        let pHandleBottomRightCorner = CGPoint(x: config.handleRect.maxX, y: config.handleRect.maxY)
+        let pHandleBottomLeftCorner = CGPoint(x: config.handleRect.minX, y: config.handleRect.maxY)
+        let pHandleTopLeftCorner = CGPoint(x: config.handleRect.minX, y: config.handleRect.minY)
+
+        let path = CGMutablePath()
+        path.move(to: CGPoint(x: config.insetBounds.minX, y: config.insetBounds.minY + config.plateRadius))
+
+        path.addArc(corner: pPlateTopLeftCorner, radius: config.plateRadius, circleComponent: .topLeft)
+        path.addArc(corner: pPlateTopRightCorner, radius: config.plateRadius, circleComponent: .topRight)
+
+        // Choose drawing technique depending on the distance between the handle's right-hand side and the plate's right-hand side.
+        if config.insetBounds.width - config.handleRect.maxX <= drawingTechniqueBufferDistance {
+            path.addLine(to: pPlateBottomRightCorner.offsetting(y: -config.plateRadius))
+            path.addCurve(to: pHandleTopRightCorner.offsetting(y: config.plateRadius), control1: pPlateBottomRightCorner, control2: pHandleTopRightCorner)
+        } else {
+            path.addArc(corner: pPlateBottomRightCorner, radius: config.plateRadius, circleComponent: .bottomRight)
+            path.addLine(to: pHandleTopRightCorner.offsetting(x: config.plateRadius))
+            path.addQuadCurve(to: pHandleTopRightCorner.offsetting(y: config.plateRadius), control: pHandleTopRightCorner)
+        }
+
+        path.addArc(corner: pHandleBottomRightCorner, radius: config.handleRadius, circleComponent: .bottomRight)
+        path.addArc(corner: pHandleBottomLeftCorner, radius: config.handleRadius, circleComponent: .bottomLeft)
+
+        // Choose drawing technique depending on the distance between the handle's left-hand side and the plate's left-hand side.
+        if config.handleRect.minX <= drawingTechniqueBufferDistance {
+            path.addLine(to: pHandleTopLeftCorner.offsetting(y: config.plateRadius))
+            path.addCurve(to: pPlateBottomLeftCorner.offsetting(y: -config.plateRadius), control1: pHandleTopLeftCorner, control2: pPlateBottomLeftCorner)
+        } else {
+            path.addLine(to: pHandleTopLeftCorner.offsetting(y: config.plateRadius))
+            path.addQuadCurve(to: pHandleTopLeftCorner.offsetting(x: -config.plateRadius), control: pHandleTopLeftCorner)
+            path.addArc(corner: pPlateBottomLeftCorner, radius: config.plateRadius, circleComponent: .bottomLeft)
+        }
+
+        path.closeSubpath()
+        return path
+    }
+}
+
+private extension CGMutablePath {
+    enum CircleComponent {
+        case topRight
+        case bottomRight
+        case bottomLeft
+        case topLeft
+
+        var startAngle: CGFloat {
+            switch self {
+            case .topRight:
+                return 3 * .pi / 2
+            case .bottomRight:
+                return 2 * .pi
+            case .bottomLeft:
+                return .pi / 2
+            case .topLeft:
+                return .pi
+            }
+        }
+
+        var endAngle: CGFloat {
+            switch self {
+            case .topRight:
+                return 2 * .pi
+            case .bottomRight:
+                return .pi / 2
+            case .bottomLeft:
+                return .pi
+            case .topLeft:
+                return 3 * .pi / 2
+            }
+        }
+    }
+
+    func addArc(corner: CGPoint, radius: CGFloat, circleComponent: CircleComponent, clockwise: Bool = false) {
+        let center: CGPoint
+        switch circleComponent {
+        case .topRight:
+            center = CGPoint(x: corner.x - radius, y: corner.y + radius)
+        case .bottomRight:
+            center = CGPoint(x: corner.x - radius, y: corner.y - radius)
+        case .bottomLeft:
+            center = CGPoint(x: corner.x + radius, y: corner.y - radius)
+        case .topLeft:
+            center = CGPoint(x: corner.x + radius, y: corner.y + radius)
+        }
+        addArc(center: center, radius: radius, circleComponent: circleComponent, clockwise: clockwise)
+    }
+
+    func addArc(center: CGPoint, radius: CGFloat, circleComponent: CircleComponent, clockwise: Bool = false) {
+        addArc(center: center, radius: radius, startAngle: circleComponent.startAngle, endAngle: circleComponent.endAngle, clockwise: clockwise)
+    }
+}
+
+private extension CGPoint {
+    func offsetting(x dx: CGFloat = 0, y dy: CGFloat = 0) -> CGPoint {
+        CGPoint(x: x + dx, y: y + dy)
+    }
+}
--- Sources/KeyboardToolbar/Views/KeyboardToolPickerBackgroundView.swift
+++ Sources/KeyboardToolbar/Views/KeyboardToolPickerBackgroundView.swift
@@ -88,13 +88,16 @@ final class KeyboardToolPickerBackgroundView: UIView {
         }
     }
 
-    private var insetBounds: CGRect {
-        return bounds.inset(by: UIEdgeInsets(top: shadowBlur, left: shadowBlur, bottom: 0, right: shadowBlur))
-    }
-    private var handleRect: CGRect {
-        let handleXPosition = preferredHandleXPosition
-        let handleOrigin = CGPoint(x: handleXPosition, y: insetBounds.maxY - handleSize.height - shadowBlur - handleShadowLength)
-        return CGRect(origin: handleOrigin, size: handleSize)
+    private var pathConfig: KeyboardToolPickerBackgroundPathConfiguration {
+        KeyboardToolPickerBackgroundPathConfiguration(
+            bounds: bounds,
+            shadowBlur: shadowBlur,
+            handleSize: handleSize,
+            preferredHandleXPosition: preferredHandleXPosition,
+            handleShadowLength: handleShadowLength,
+            plateRadius: plateRadius,
+            handleRadius: handleRadius
+        )
     }
 
     override init(frame: CGRect) {
@@ -110,8 +113,8 @@ final class KeyboardToolPickerBackgroundView: UIView {
         super.draw(rect)
         let context = UIGraphicsGetCurrentContext()
         context?.clear(rect)
-        if let context = context {
-            let path = makeBackgroundPath()
+        if let context {
+            let path = CGPath.keyboardToolPickerBackground(with: pathConfig)
             drawShadow(of: path, to: context)
             drawHandleShadow(to: context)
             drawFill(of: path, to: context)
@@ -136,7 +139,7 @@ private extension KeyboardToolPickerBackgroundView {
                 UIColor.black.withAlphaComponent(1).cgColor,
                 UIColor.black.withAlphaComponent(0).cgColor
             ]
-            let startLocation = (bounds.height - shadowBlur - handleRect.height / 2) / bounds.height
+            let startLocation = (bounds.height - shadowBlur - pathConfig.handleRect.height / 2) / bounds.height
             let endLocation = (bounds.height - shadowBlur) / bounds.height
             let locations: [CGFloat] = [1 - startLocation, 1 - endLocation]
             let gradient = CGGradient(colorsSpace: colorSpace, colors: colors as CFArray, locations: locations)!
@@ -163,8 +166,8 @@ private extension KeyboardToolPickerBackgroundView {
 
     private func drawHandleShadow(to context: CGContext) {
         if let handleShadowColor = handleShadowColor, handleShadowLength > 0 {
-            let handlePath = UIBezierPath(roundedRect: handleRect, cornerRadius: handleRadius)
-            let handleShadowRect = handleRect.offsetBy(dx: 0, dy: handleShadowLength)
+            let handlePath = UIBezierPath(roundedRect: pathConfig.handleRect, cornerRadius: handleRadius)
+            let handleShadowRect = pathConfig.handleRect.offsetBy(dx: 0, dy: handleShadowLength)
             let handleShadowPath = UIBezierPath(roundedRect: handleShadowRect, cornerRadius: handleRadius)
             context.saveGState()
             // Draw the shadow
@@ -197,111 +200,5 @@ private extension KeyboardToolPickerBackgroundView {
         context.addPath(path)
         context.strokePath()
         context.restoreGState()
-    }
-
-    private func makeBackgroundPath() -> CGPath {
-        let drawingTechniqueBufferDistance = handleSize.width * 0.8
-
-        let pPlateTopLeftCorner = CGPoint(x: insetBounds.minX, y: insetBounds.minY)
-        let pPlateTopRightCorner = CGPoint(x: insetBounds.maxX, y: insetBounds.minY)
-        let pPlateBottomRightCorner = CGPoint(x: insetBounds.maxX, y: handleRect.minY)
-        let pPlateBottomLeftCorner = CGPoint(x: insetBounds.minX, y: handleRect.minY)
-
-        let pHandleTopRightCorner = CGPoint(x: handleRect.maxX, y: handleRect.minY)
-        let pHandleBottomRightCorner = CGPoint(x: handleRect.maxX, y: handleRect.maxY)
-        let pHandleBottomLeftCorner = CGPoint(x: handleRect.minX, y: handleRect.maxY)
-        let pHandleTopLeftCorner = CGPoint(x: handleRect.minX, y: handleRect.minY)
-
-        let path = CGMutablePath()
-        path.move(to: CGPoint(x: insetBounds.minX, y: insetBounds.minY + plateRadius))
-
-        path.addArc(corner: pPlateTopLeftCorner, radius: plateRadius, circleComponent: .topLeft)
-        path.addArc(corner: pPlateTopRightCorner, radius: plateRadius, circleComponent: .topRight)
-
-        // Choose drawing technique depending on the distance between the handle's right-hand side and the plate's right-hand side.
-        if insetBounds.width - handleRect.maxX <= drawingTechniqueBufferDistance {
-            path.addLine(to: pPlateBottomRightCorner.offsetting(y: -plateRadius))
-            path.addCurve(to: pHandleTopRightCorner.offsetting(y: plateRadius), control1: pPlateBottomRightCorner, control2: pHandleTopRightCorner)
-        } else {
-            path.addArc(corner: pPlateBottomRightCorner, radius: plateRadius, circleComponent: .bottomRight)
-            path.addLine(to: pHandleTopRightCorner.offsetting(x: plateRadius))
-            path.addQuadCurve(to: pHandleTopRightCorner.offsetting(y: plateRadius), control: pHandleTopRightCorner)
-        }
-
-        path.addArc(corner: pHandleBottomRightCorner, radius: handleRadius, circleComponent: .bottomRight)
-        path.addArc(corner: pHandleBottomLeftCorner, radius: handleRadius, circleComponent: .bottomLeft)
-
-        // Choose drawing technique depending on the distance between the handle's left-hand side and the plate's left-hand side.
-        if handleRect.minX <= drawingTechniqueBufferDistance {
-            path.addLine(to: pHandleTopLeftCorner.offsetting(y: plateRadius))
-            path.addCurve(to: pPlateBottomLeftCorner.offsetting(y: -plateRadius), control1: pHandleTopLeftCorner, control2: pPlateBottomLeftCorner)
-        } else {
-            path.addLine(to: pHandleTopLeftCorner.offsetting(y: plateRadius))
-            path.addQuadCurve(to: pHandleTopLeftCorner.offsetting(x: -plateRadius), control: pHandleTopLeftCorner)
-            path.addArc(corner: pPlateBottomLeftCorner, radius: plateRadius, circleComponent: .bottomLeft)
-        }
-
-        path.closeSubpath()
-        return path
-    }
-}
-
-private extension CGPoint {
-    func offsetting(x dx: CGFloat = 0, y dy: CGFloat = 0) -> CGPoint {
-        return CGPoint(x: x + dx, y: y + dy)
-    }
-}
-
-private extension CGMutablePath {
-    enum CircleComponent {
-        case topRight
-        case bottomRight
-        case bottomLeft
-        case topLeft
-
-        var startAngle: CGFloat {
-            switch self {
-            case .topRight:
-                return 3 * .pi / 2
-            case .bottomRight:
-                return 2 * .pi
-            case .bottomLeft:
-                return .pi / 2
-            case .topLeft:
-                return .pi
-            }
-        }
-
-        var endAngle: CGFloat {
-            switch self {
-            case .topRight:
-                return 2 * .pi
-            case .bottomRight:
-                return .pi / 2
-            case .bottomLeft:
-                return .pi
-            case .topLeft:
-                return 3 * .pi / 2
-            }
-        }
-    }
-
-    func addArc(corner: CGPoint, radius: CGFloat, circleComponent: CircleComponent, clockwise: Bool = false) {
-        let center: CGPoint
-        switch circleComponent {
-        case .topRight:
-            center = CGPoint(x: corner.x - radius, y: corner.y + radius)
-        case .bottomRight:
-            center = CGPoint(x: corner.x - radius, y: corner.y - radius)
-        case .bottomLeft:
-            center = CGPoint(x: corner.x + radius, y: corner.y - radius)
-        case .topLeft:
-            center = CGPoint(x: corner.x + radius, y: corner.y + radius)
-        }
-        addArc(center: center, radius: radius, circleComponent: circleComponent, clockwise: clockwise)
-    }
-
-    func addArc(center: CGPoint, radius: CGFloat, circleComponent: CircleComponent, clockwise: Bool = false) {
-        addArc(center: center, radius: radius, startAngle: circleComponent.startAngle, endAngle: circleComponent.endAngle, clockwise: clockwise)
     }
 }
